6e26eed6ed1f8e5170085c2f14839a54dbb8f676
MadddcorE <57013959+MadddcorE@users.noreply.github.com>
2025-07-06 13:22:10 +0000

    Added: config.py + telegram tuts

---
+++ telegram-buttons-tutorial.py
@@ -0,0 +1,50 @@
+import requests
+from time import sleep
+
+global OFFSET
+OFFSET = 0
+
+botToken = "7684765245:AAGrxdSI_YFU9m7AhcK3Ma3eGAMeud0iFMI"
+
+global requestURL
+global sendURL
+
+requestURL = "https://api.telegram.org/bot" + botToken + "/getUpdates"
+sendURL = "https://api.telegram.org/bot" + botToken + "/sendMessage"
+
+def update(url):
+    global OFFSET
+
+    try:
+        update_raw = requests.get(url + "?offset=" + str(OFFSET))
+        update = update_raw.json()
+        result = extract_results(update)
+
+        if result != False:
+
+            OFFSET = result['update_id'] + 1
+
+            chat_id = result['message']['chat']['id']
+            text = result['message']['text']
+
+            send_message(chat_id, text)
+        except requests.exceptions.ConnectionError:
+            pass
+
+def extract_results(dict):
+    result_array = dict['result']
+
+
+    if result_array == []:
+        return False
+    else:
+        result_dic = result_array[0]
+        return result_dic
+
+def send_message(chatId, message):
+    requests.post(sendURL + "?chat_id=" + str(chatId) + "&text=" + message)
+
+
+while True:
+    update (requestURL)
+    sleep(1)
